317339c7cd9b5f38542f8a5d36e0dc3336c34ac1
Aaron Wilson <aaron.wi@gmail.com>
2017-07-17 22:20:22 +0100

    - Added empty implementation for the asset file reader

--- include/jsonassetfilereader.h
+++ include/jsonassetfilereader.h
@@ -4,6 +4,7 @@
 
 #include "jsonassetfilecommon.h"
 #include <string>
+#include <vector>
 
 
 namespace jaf
@@ -14,7 +15,33 @@ namespace jaf
     {
         public:
 
-            Reader( std::string filePath );
+            struct Asset
+            {
+                std::vector<unsigned char>  data;
+                size_t          dataLength = -1;
+                std::string     prefix;
+                std::string     name;
+            };
+
+                                Reader( const char* fileName );
+                                Reader( std::string filePath );
+                                ~Reader();
+
+            bool                start();
+            bool                getNext( Asset& asset_OUT );
+            bool                finish();
+            bool                isOpen();
+            std::string         lastError();
+
+
+        private:
+
+            void                setError( const char* error );
+
+            std::string*        mFileName   = nullptr;
+            std::string*        mLastError  = nullptr;
+            std::ifstream*      mFile       = nullptr;
+
 
 
     }; // Reader
--- src/jsonassetfilereader.cpp
+++ src/jsonassetfilereader.cpp
@@ -1,8 +1,59 @@
 #include <jsonassetfilereader.h>
 #include <jsonassetfileutils.h>
+#include <fstream>
 
+
+jaf::Reader::Reader( const char* fileName )
+    : mFileName( new std::string( fileName ) ),
+      mLastError( new std::string( "" ) ),
+      mFile( new std::ifstream() )
+{
+}
 
 jaf::Reader::Reader( std::string filePath )
+    : Reader( filePath.c_str() )
+{
+}
+
+jaf::Reader::~Reader()
+{
+    finish();
+    delete mFileName;
+    delete mLastError;
+    delete mFile;
+}
+
+bool jaf::Reader::start()
+{
+
+    return false;
+}
+
+bool jaf::Reader::getNext( Asset& asset_OUT )
+{
+
+    return false;
+}
+
+bool jaf::Reader::finish()
+{
+
+    return false;
+}
+
+bool jaf::Reader::isOpen()
+{
+
+    return false;
+}
+
+std::string jaf::Reader::lastError()
+{
+
+    return *mLastError;
+}
+
+void jaf::Reader::setError( const char* error )
 {
 
 }
